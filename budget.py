efc1db2db439ad396f14c66b043cfe9d5ee64c92
daniel.boros <daniel.boros@ocado.com>
2021-04-30 11:09:28 +0100

    Implement withdraw, transfer, get_balance, check_funds methods + update deposit method

--- budget.py
+++ budget.py
@@ -5,25 +5,30 @@ class Category:
         self.ledger = []
 
     def deposit(self, amount, description=""):
-        deposit = {"amount": amount, "description": description}
-        self.ledger.append(deposit)
+        self.ledger.append({"amount": amount, "description": description})
 
     def withdraw(self, amount, description=""):
-        isEnough = self.check_funds(amount)
-        obj = {"amount": -amount, "description": description}
-        self.ledger.append(obj)
-        # ToDo: Check Ledger if enough in ledger
-        return False
+
+        if self.check_funds(amount):
+            self.ledger.append({"amount": -amount, "description": description})
+            return True
+        else:
+            return False
 
     def get_balance(self):
-        return "balance of current Category"
+        return sum([log.amount for log in self.ledger])
     
-    def transfer(self, amount, other):
-        isEnough = self.check_funds(amount)
-        return False
+    def transfer(self, amount, toOther: Category):
+
+        if self.check_funds(amount):
+            self.withdraw(amount, f"Transfer to {toOther.name}")
+            toOther.deposit(amount, f"Transfer from {self.name}")
+            return True
+        else:
+            return False
 
     def check_funds(self, amount):
-        return False
+        return amount <= self.get_balance()
     
     def __repr__(self):
         return ""
